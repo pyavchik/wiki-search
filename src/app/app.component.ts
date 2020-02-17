d7b32c0309751d165dd44771d613824e2ff2ad15
pyavchik <pyavchik@gmail.com>
2020-02-17 19:00:02 +0200

    child to parent communication added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wiki-search';
+
+  onTerm(term: string) {
+    console.log('I am the app and here is the term');
+  }
 }
