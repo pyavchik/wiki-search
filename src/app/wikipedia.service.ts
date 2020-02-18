15d40936e49986dfbd486314cd17dc989a464ef8
pyavchik <pyavchik@gmail.com>
2020-02-18 08:07:51 +0200

    accessing service added

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  public search(term: string) {
+    return 'I am wiki search results';
+  }
 }
